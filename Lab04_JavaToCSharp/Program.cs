452234627c84de0e2a478805a1a03e2ddd414953
Andrew Harkins <extremelyexceeding@gmail.com>
2018-12-18 19:23:41 -0600

    Lab04 - Get user iput & display - Console Application
    Get the user input check if input is number,  If input '-99' stop program and display results,
    Display biggest number,
    Display smallest number,

--- Lab04_JavaToCSharp/Program.cs
+++ Lab04_JavaToCSharp/Program.cs
@@ -10,6 +10,63 @@ namespace Lab04_JavaToCSharp
     {
         static void Main(string[] args)
         {
+            // Program to enter a series of integers and display 'Largest' and 'Smallest'
+            string input;
+            bool result = false;
+            int smallest = 0;
+            int biggest = 0;
+            int count = 0;
+
+            // Loop the program to get multiple numbers
+            while (!result)
+            {
+                // Write to user and get the users input.
+                Console.WriteLine("Enter a list of numbers, one number at a time.");
+                input = Console.ReadLine();
+
+                // If user enters '-99' end the program and show results.
+                if (!input.Equals("-99"))
+                {
+                    // Check to make sure the user input is a number.
+                    if (Int32.TryParse(input, out int hold))
+                    {
+                        // On very first pass set biggest and smallest defaults to input.
+                        if (count == 0)
+                        {
+                            smallest = hold;
+                            biggest = hold;
+                            count++;
+                        }
+
+                        if (hold < smallest)
+                        {
+                            smallest = hold;    // Get the smallest value.
+                        }
+                        else if (hold > biggest)    // Get the biggest value.
+                        {
+                            biggest = hold;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("User input must be a whole number(Integer), Error input not number" +
+                                                 "\nPress any key to exit the program and continue.");
+                        result = true;
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    //Show biggest and smallest numbers.
+                    Console.WriteLine("\nHere are the biggest and smallest numbers from the user input");
+                    Console.WriteLine("Biggest: " + biggest);
+                    Console.WriteLine("Smallest: " + smallest);
+                    Console.WriteLine();
+                    Console.WriteLine("\nPress any key to exit the program and continue.");
+                    Console.ReadKey();
+                    result = true;
+                }
+            }         
         }
     }
 }
